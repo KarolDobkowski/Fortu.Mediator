8646b179dd9140b6a822ddfb13e5834fec59bb29
Karol Dobkowski <karoldobkowski3@gmail.com>
2018-11-30 23:14:25 +0100

    add passing test for pipeline

---
+++ Flowem.Mediator.Tests/PassingTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Flowem.Mediator.Tests
+{
+    public class PassingTests
+    {
+        [Fact]
+        public void Pass()
+        {
+            Assert.True(true);
+        }
+    }
+}
