798875fce25aedba9eec907ba53f08cd55df47da
Karol Dobkowski <karoldobkowski3@gmail.com>
2018-12-06 23:42:12 +0100

    add void Tasks test

---
+++ Flowem.Mediator.Tests/Messages/MessagesTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Flowem.Mediator.Core.Interfaces;
+using Flowem.Mediator.Tests.Samples;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Flowem.Mediator.Tests.Messages
+{
+    public class MessagesTests
+    {
+        protected readonly IMediator _mediator;
+        public static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1);
+
+        public MessagesTests()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<IMessageHandler<SimpleMessage>, SimpleMessageHandler>();
+            var provider = services.BuildServiceProvider();
+
+            _mediator = new Core.Mediator(provider);
+        }
+
+        [Fact]
+        public async Task ShouldSendMessagesTwice()
+        {
+            SimpleMessage._counter = 0;
+            var shouldBe_0 = SimpleMessage._counter;
+            await _semaphoreSlim.WaitAsync();
+            _mediator.Send(new SimpleMessage());
+            await _semaphoreSlim.WaitAsync();
+            _mediator.Send(new SimpleMessage());
+            await _semaphoreSlim.WaitAsync();
+            var shouldBe_2 = SimpleMessage._counter;
+            _semaphoreSlim.Release();
+
+            Assert.Equal(0, shouldBe_0);
+            Assert.Equal(2, shouldBe_2);
+        }
+
+    }
+}
