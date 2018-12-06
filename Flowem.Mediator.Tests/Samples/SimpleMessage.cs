af3fea809a3bb602739a97b514ac48e10e8c3434
Karol Dobkowski <karoldobkowski3@gmail.com>
2018-12-06 23:45:02 +0100

    add missing files

---
+++ Flowem.Mediator.Tests/Samples/SimpleMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+using Flowem.Mediator.Core.Interfaces;
+
+namespace Flowem.Mediator.Tests.Samples
+{
+    public class SimpleMessage : IMessage
+    {
+        public static int _counter { get; set; }
+
+        public int Counter
+        {
+            get => _counter; 
+            set => _counter = value;
+        }
+    }
+}
---
+++ Flowem.Mediator.Tests/Samples/SimpleMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Flowem.Mediator.Core.Interfaces;
+using Flowem.Mediator.Tests.Messages;
+
+namespace Flowem.Mediator.Tests.Samples
+{
+    public class SimpleMessageHandler : IMessageHandler<SimpleMessage>
+    {
+        public Task Handle(SimpleMessage message)
+        {
+            message.Counter++;
+            MessagesTests._semaphoreSlim.Release();
+            return Task.CompletedTask;
+        }
+    }
+}
