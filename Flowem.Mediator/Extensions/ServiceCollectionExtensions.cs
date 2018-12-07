a091bbf93dcd2e2129611a6ecfe067b6818ac8f9
Karol Dobkowski <karoldobkowski3@gmail.com>
2018-12-07 19:59:50 +0100

    add service collection extension

---
+++ Flowem.Mediator/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flowem.Mediator.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services)
+            => services.AddScoped<IMediator, Mediator>();
+    }
+}
